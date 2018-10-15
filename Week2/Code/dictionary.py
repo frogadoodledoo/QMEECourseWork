267d91410f7ef925a4a54c2ccfe3712c53231573
frogadoodledoo <merrycrowson@googlemail.com>
2018-10-15 15:39:27 -0400

    catching up on Practical 1 week 2

--- Week2/Code/dictionary.py
+++ Week2/Code/dictionary.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+
+""" Populates a dictionary of order names and taxa. 
+Able to index multiple values to a key
+Practical 1, Week 2 """
+
+__author__ = 'Merry Crowson'
+__version__ = '0.0.1'
+
+
+# data
+
 taxa = [ ('Myotis lucifugus','Chiroptera'),
          ('Gerbillus henleyi','Rodentia',),
          ('Peromyscus crinitus', 'Rodentia'),
@@ -20,3 +32,22 @@ taxa = [ ('Myotis lucifugus','Chiroptera'),
 # SAYS WHAT THE SCRIPT DOES AND WHO THE AUTHOR IS
 
 # Write your script here:
+
+taxa_dic = {}
+
+for value, key in taxa:
+        #Initialize empty list if key doesn't exist
+        if key not in taxa_dic:
+                taxa_dic[key] = []
+        # Now we know the key always exists
+        taxa_dic[key].append(value)
+print(taxa_dic)
+
+
+#how to do it as conditional statement?
+#taxa_dic = dict({t[1]:set([t[0]]) for t in taxa}) # Only one value per key
+#print(taxa_dic)
+
+
+#dict([(a,b) for b, a in taxa])
+
--- Week2/Code/lc1.py
+++ Week2/Code/lc1.py
@@ -1,17 +1,3 @@
-
-
-#(1) Write three separate list comprehensions that create three different
-# lists containing the latin names, common names and mean body masses for
-# each species in birds, respectively. 
-
-# (2) Now do the same using conventional loops (you can shoose to do this 
-# before 1 !). 
-
-# ANNOTATE WHAT EVERY BLOCK OR IF NECESSARY, LINE IS DOING! 
-
-# ALSO, PLEASE INCLUDE A DOCSTRING AT THE BEGINNING OF THIS FILE THAT 
-# SAYS WHAT THE SCRIPT DOES AND WHO THE AUTHOR IS.
-
 #!/usr/bin/env python3
 
 """ Creates seperate lists of latin names, common names and mean body masses 
@@ -35,34 +21,46 @@ birds = ( ('Passerculus sandwichensis','Savannah sparrow',18.7),
 #(1) list comprehensions
 
 # Create list of latin names of each species in birds
-latin_lc = set([species[0] for species in birds]) #takes the first element [0] from each of the tuples (species) within the tuple "birds"
+latin_lc = list([species[0] for species in birds]) #Takes the first element [0] from each of the tuples (species) within the tuple "birds"
 print(latin_lc) #returns a set
 
 # Create list of common names of each species in birds
-latin_lc = set([species[1] for species in birds])
-print(latin_lc)
-
+common_lc = list([species[1] for species in birds])
+print(common_lc)
 
 # Create list of mean body masses of each species in birds
-latin_lc = set([species[2] for species in birds])
-print(latin_lc)
+bodymass_lc = list([species[2] for species in birds])
+print(bodymass_lc)
 
 #(2) For loops
 
 # For loop of latin names of each species in birds
-latin_loops = set() # initialise set
+latin_loops = list() # initialise set
 for species in birds:
-    latin_loops.add(species[0]) #Takes the first element and appends it to the set "latin loops"
+    latin_loops.append(species[0]) #Takes the first element and appends it to the list "latin loops"
 print(latin_loops)
 
 # For loop of common names of each species in birds
-latin_loops = set()
+common_loops = list()
 for species in birds:
-    latin_loops.add(species[1])
-print(latin_loops)
+    common_loops.append(species[1])
+print(common_loops)
 
 # For loop of the mean body mass for each species in birds
-latin_loops = set()
+bodymass_loops = list()
 for species in birds:
-    latin_loops.add(species[2])
-print(latin_loops)
+    bodymass_loops.append(species[2])
+print(bodymass_loops)
+
+
+#(1) Write three separate list comprehensions that create three different
+# lists containing the latin names, common names and mean body masses for
+# each species in birds, respectively. 
+
+# (2) Now do the same using conventional loops (you can shoose to do this 
+# before 1 !). 
+
+# ANNOTATE WHAT EVERY BLOCK OR IF NECESSARY, LINE IS DOING! 
+
+# ALSO, PLEASE INCLUDE A DOCSTRING AT THE BEGINNING OF THIS FILE THAT 
+# SAYS WHAT THE SCRIPT DOES AND WHO THE AUTHOR IS.
